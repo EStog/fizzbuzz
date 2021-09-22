572fd91b712dde9c3726141c4cd4e41408f8b042
Ernesto Soto Gómez <esto.yinyang@gmail.com>
2021-09-22 18:02:55 -0400

    Update docs
    Fixes #3

--- fizzbuzz_cli.py
+++ fizzbuzz_cli.py
@@ -5,7 +5,8 @@ app = typer.Typer()
 
 @app.command()
 def classic_fizzbuzz(n: int):
-    """Run the classsic fizzbuzz"""
+    """Run the classsic fizzbuzz
+    """
     typer.echo_via_pager(f'{s} ' for s in fizzbuzz.classic_fizzbuzz(n))
 
 if __name__ == '__main__':
