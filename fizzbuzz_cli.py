3e1017070298ff90fbc1f385671b678447a513b0
Ernesto Soto Gómez <esto.yinyang@gmail.com>
2021-09-22 17:48:15 -0400

    Add CLI Issue: #3

---
+++ fizzbuzz_cli.py
@@ -0,0 +1,12 @@
+import typer
+import fizzbuzz
+
+app = typer.Typer()
+
+@app.command()
+def classic_fizzbuzz(n: int):
+    """Run the classsic fizzbuzz"""
+    typer.echo_via_pager(f'{s} ' for s in fizzbuzz.classic_fizzbuzz(n))
+
+if __name__ == '__main__':
+    app()
